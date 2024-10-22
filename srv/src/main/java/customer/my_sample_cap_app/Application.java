9ec590ae0cc1d72b24b76088c7f8a568e4c997f8
Gaurav Abbi <gaurav.abbi@sap.com>
2024-10-22 15:12:03 +0200

    pre-deploy artifacts

---
+++ srv/src/main/java/customer/my_sample_cap_app/Application.java
@@ -0,0 +1,13 @@
+package customer.my_sample_cap_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
